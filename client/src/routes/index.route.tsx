57e90f4ee04a0ef494e834dc9aa1b3fc166881aa
dangtoan <dangtoan.2k6@gmail.com>
2025-12-09 12:08:31 +0700

    update

--- client/src/routes/index.route.tsx
+++ client/src/routes/index.route.tsx
@@ -1,48 +1,47 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter } from "react-router-dom";
 
-import Layout from '../Layout/layout';
-import News from '../pages/News';
-import Payment from '../components/Payment';
-import NotFoundPage from '../pages/NotFoundPage';
-import MovieCalendar from '../pages/MovieCalendar';
-import HomePage from '../pages/HomePage';
+import Layout from "../Layout/layout";
+import News from "../pages/News";
+import Payment from "../components/Payment";
+import NotFoundPage from "../pages/NotFoundPage";
+import MovieCalendar from "../pages/MovieCalendar";
+import HomePage from "../pages/HomePage";
 
 export const routers = createBrowserRouter([
-    {
-        path: '/news',
-        element: (
-            <Layout>
-                <News></News>
-                <div></div>
-            </Layout>
-        ),
-    },
-    {
-        path: '/payment',
-        element: (
-            <Layout>
-                <Payment />
-            </Layout>
-        ),
-    },
-    {
-        path: '/movie-calendar',
-        element: (
-            <Layout>
-                <MovieCalendar />
-            </Layout>
-        ),
-    },
-    {
-        path: '/home',
-        element: (
-            <Layout>
-                <HomePage />
-            </Layout>
-        ),
-    },
-    {
-        path: '*',
-        element: <NotFoundPage />,
-    },
+  {
+    path: "/news",
+    element: (
+      <Layout>
+        <News></News>
+      </Layout>
+    ),
+  },
+  {
+    path: "/payment",
+    element: (
+      <Layout>
+        <Payment />
+      </Layout>
+    ),
+  },
+  {
+    path: "/movie-calendar",
+    element: (
+      <Layout>
+        <MovieCalendar />
+      </Layout>
+    ),
+  },
+  {
+    path: "/",
+    element: (
+      <Layout>
+        <HomePage />
+      </Layout>
+    ),
+  },
+  {
+    path: "*",
+    element: <NotFoundPage />,
+  },
 ]);
